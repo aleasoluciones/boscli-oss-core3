246734d22c52f780d6bc25ca58e94a40216d5cda
eferro <eduardo.ferro.aldama@gmail.com>
2009-09-30 10:31:10 +0000

    Branch to develop support for stacable context (like in Cisco clis)
    git-svn-id: http://oss.alea-soluciones.com/svn/boscli-oss/boscli-oss-core@226 44b62038-78f7-4aed-b65b-4fa73381d21b

---
+++ branches/context-stack/src/boscli/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# (c) Alea-Soluciones (Bifer) 2007, 2008, 2009
+# Licensed under GPL v3 or later, see COPYING for the whole text
+
+
+import boscli
+
+def main():
+    boscli.main()
